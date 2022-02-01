7a02309e45ee53d835353a09ee2864c345a4a0f8
Chinonso <benokafor67@gmail.com>
2022-02-01 17:40:17 +0100

    added local storage functionality to the app

--- app.js
+++ app.js
@@ -2,13 +2,24 @@ let myLeads = [];
 const inputEl = document.getElementById("input-el");
 const ulEl = document.getElementById("ul-el")
 
+let leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
+
+if (leadsFromLocalStorage) {
+    myLeads = leadsFromLocalStorage
+    renderLeads()
+}
+
+
 const inputBtn = document.getElementById("input-btn");
 
 inputBtn.addEventListener("click", () => {
     myLeads.push(inputEl.value)
 //   console.log(myLeads);
     inputEl.value = "";
+
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
     renderLeads()
+    console.log( localStorage.getItem("myLeads") )
 });
 
 function renderLeads() {
