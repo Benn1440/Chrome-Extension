a2858a5697f1928fc82c962e56a92d7f2fceccef
Chinonso <benokafor67@gmail.com>
2022-02-09 17:49:41 +0100

    manipulating  localstorage

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
 let myLeads = [];
 const inputEl = document.getElementById("input-el");
 const ulEl = document.getElementById("ul-el")
+const deleteBtn = document.getElementById("delete-btn")
 
-let leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
+const leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
+
+
 
 if (leadsFromLocalStorage) {
     myLeads = leadsFromLocalStorage
     renderLeads()
 }
+
+deleteBtn.addEventListener("dblclick", function() {
+   localStorage.clear()
+   myLeads = []
+   renderLeads()
+})
 
 
 const inputBtn = document.getElementById("input-btn");
